32e7f65c09be487140e257367b44a09dcc966cbe
szk-9274 <szk23b0702@gmail.com>
2026-02-09 23:45:43 +0900

    move old app

--- renderer/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-plugins: [
-require('tailwindcss'), // ← v3 の公式プラグイン
-require('autoprefixer'),
-],
-};
--- src/preload.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('win', {
-minimize: () => ipcRenderer.send('win:minimize'),
-close: () => ipcRenderer.send('win:close'),
-});
-contextBridge.exposeInMainWorld('gitlab', {
-openProjectWeb: () => ipcRenderer.invoke('gitlab:openProjectWeb'),
-});
